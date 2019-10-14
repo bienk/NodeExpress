585efabc84d0c5ef23c7f9ed23d05fb7bdeb321c
bienk <bianchi.andrea@iisgalvanimi.edu.it>
2019-10-14 09:41:45 +0000

    quasi finito

--- EXPRESS_STATIC_FILES/app.js
+++ EXPRESS_STATIC_FILES/app.js
@@ -3,6 +3,10 @@ var app = express();
 app.get('/',function(req,res){
  res.sendFile(path.join(__dirname+'/index.html')); //__dirname : Ritorna la cartella del progetto
 });
+app.get('/',function(req,res){
+ res.sendFile(path.join(__dirname+'/about.html')); //__dirname : Ritorna la cartella del progetto
+});
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
